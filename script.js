db7fd16de8925ed296d530b2c7f62a0e941ba7a8
jery Mey <jerymey25@gmqil.com>
2023-07-25 09:14:10 +0100

    Edit Js file

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamb = document.querySelector(".hamb")
-const navLinks = document.querySelector(".nav-links");
-
-hamb.addEventListener("click", () => {
-    hamb.classList.toggle("active");
-    navLinks.classList.toggle("active");
-})
