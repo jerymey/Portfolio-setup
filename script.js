d320b2e0283a0857323684757d271b7511c8aae0
jery Mey <jerymey25@gmqil.com>
2023-07-25 10:39:11 +0100

    Add click event

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ hamb.addEventListener("click", () => {
     hamb.classList.toggle("active");
     navLinks.classList.toggle("active");
 })
+
+document.querySelectorAll(".navlink").forEach(n => n.
+    addEventListener("click", () => {
+        hamb.classList.remove("active");
+        navLinks.classList.remove("active");
+    }))
