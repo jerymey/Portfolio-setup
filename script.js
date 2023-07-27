d3ade9abccf430f9e9e9d138124d76e685854d85
Jery Mey <jerymey25@gmail.com>
2023-07-27 08:36:06 +0100

    update style.css

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ document.querySelectorAll('.navlink').forEach((n) => n
     hamb.classList.remove('active');
     navLinks.classList.remove('active');
   }));
+
+  // contact form
+const formula = document.querySelector('.formClass');
+function errData(data) {
+  document.getElementById('error').innerHTML = data;
+}
+const email = document.getElementById('contact-email');
+formula.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
+    errData('Please input email in lower case');
+    e.preventDefault();
+    return false;
+  }
+  return true;
+});
