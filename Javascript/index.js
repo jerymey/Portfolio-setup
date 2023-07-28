3eefa8ae38a251be34759c19df087149a28e96fe
Jery Mey <jerymey25@gmail.com>
2023-07-28 11:38:14 +0100

    Use Js object

---
+++ Javascript/index.js
@@ -0,0 +1,57 @@
+const projects = [
+    {
+      id: 1,
+      title: 'Topic',
+      name: 'CANOPY',
+      back: 'Back End Dev',
+      age: 12,
+      SourceLink: 'https://github.com/jerymey',
+      LiveLink: 'https://jerymey.github.io/Portfolio-setup/',
+      years: '2015',
+      Image: 'images/art.png',
+      descrip: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    },
+    {
+      id: 2,
+      title: 'Multi-Post Stories',
+      name: 'FACEBOOK',
+      back: 'Full Stack Dev',
+      SourceLink: 'https://github.com/jerymey',
+      LiveLink: 'https://jerymey.github.io/Portfolio-setup/',
+      years: '2015',
+      Image: 'images/tonic.png',
+      descrip: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    },
+    {
+      id: 3,
+      title: 'Facebook 360',
+      name: 'FACEBOOK',
+      back: 'Full Stack Dev',
+      SourceLink: 'https://github.com/jerymey',
+      LiveLink: 'https://jerymey.github.io/Portfolio-setup/',
+      years: '2015',
+      Image: 'images/yoga.png',
+      descrip: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    },
+    {
+      id: 4,
+      title: 'Uber Navigation',
+      name: 'Uber',
+      back: 'Lead Developer', 
+      SourceLink: 'https://github.com/jerymey',
+      LiveLink: 'https://jerymey.github.io/Portfolio-setup/',
+      years: '2018',
+      Image: 'images/yoga.png',
+      descrip: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    }
+  ];
+
+  let htmlCode = ``;
+  projects.forEach(function(SingleProjectsObjects) {
+
+    htmlCode =
+    htmlCode +
+  
+  
+    
+  });
